2a5dc4f58ab113ad8fcc5e7ba2b4fb81af3008c5
jhon.cordoba <cordoba1923@gmail.com>
2025-02-24 18:01:48 +0000

    Added Test deployment

--- app-cdk/app_cdk/pipeline_cdk_stack.py
+++ app-cdk/app_cdk/pipeline_cdk_stack.py
@@ -8,12 +8,11 @@ from aws_cdk import (
     aws_codebuild as codebuild,
     aws_codepipeline_actions as codepipeline_actions,
     aws_iam as iam,
-    aws_ssm as ssm,
 )
 
 class PipelineCdkStack(Stack):
 
-    def __init__(self, scope: Construct, id: str, ecr_repository, **kwargs) -> None:
+    def __init__(self, scope: Construct, id: str, ecr_repository, test_app_fargate, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # Creates a CodeConnections resource called 'CICD_Workshop_Connection'
@@ -49,20 +48,20 @@ class PipelineCdkStack(Stack):
                 environment_variables = {
                     'IMAGE_TAG': codebuild.BuildEnvironmentVariable(
                         type = codebuild.BuildEnvironmentVariableType.PLAINTEXT,
-                        value = 'latest',
+                        value = 'latest'
                     ),
                     'IMAGE_REPO_URI': codebuild.BuildEnvironmentVariable(
                         type = codebuild.BuildEnvironmentVariableType.PLAINTEXT,
-                        value = ecr_repository.repository_uri,
+                        value = ecr_repository.repository_uri
                     ),
                     'AWS_DEFAULT_REGION': codebuild.BuildEnvironmentVariable(
                         type = codebuild.BuildEnvironmentVariableType.PLAINTEXT,
-                        value = os.environ['CDK_DEFAULT_REGION'],
+                        value = os.environ['CDK_DEFAULT_REGION']
                     )
                 }
             ),
         )
-   
+
         docker_build_project.add_to_role_policy(iam.PolicyStatement(
             effect = iam.Effect.ALLOW,
             actions = [
@@ -77,36 +76,11 @@ class PipelineCdkStack(Stack):
                 'ecr:InitiateLayerUpload',
                 'ecr:UploadLayerPart',
                 'ecr:CompleteLayerUpload',
-                'ecr:PutImage',
+                'ecr:PutImage'
             ],
             resources = ['*'],
         ))
 
-        ssmParameter = ssm.StringParameter(
-            self, 'SignerProfileARN',
-            parameter_name='signer-profile-arn',
-            string_value='arn:aws:signer:us-east-2:188788289838:/signing-profiles/ecr_signing_profile'
-        )
-   
-        docker_build_project.add_to_role_policy(iam.PolicyStatement(
-            effect = iam.Effect.ALLOW,
-            actions = [
-                'ssm:GetParametersByPath',
-                'ssm:GetParameters',
-            ],
-            resources = ['*'],
-        ))
-
-        docker_build_project.add_to_role_policy(iam.PolicyStatement(
-            effect = iam.Effect.ALLOW,
-            actions = [
-                'signer:PutSigningProfile',
-                'signer:SignPayload',
-                'signer:GetRevocationStatus'
-            ],
-            resources = ['*'],
-        ))
-   
         source_output = codepipeline.Artifact()
         unit_test_output = codepipeline.Artifact()
         docker_build_output = codepipeline.Artifact()
@@ -130,24 +104,35 @@ class PipelineCdkStack(Stack):
             action_name = 'Unit-Test',
             project = code_quality_build,
             input = source_output,  # The build action must use the CodeStarConnectionsSourceAction output as input.
-            outputs = [unit_test_output],
+            outputs = [unit_test_output]
         )
 
         pipeline.add_stage(
-            stage_name = 'Code-Unit-Tests',
-            actions = [build_action],
+            stage_name = 'Code-Quality-Testing',
+            actions = [build_action]
         )
 
         docker_build_action = codepipeline_actions.CodeBuildAction(
             action_name = 'Docker-Build',
             project = docker_build_project,
             input = source_output,
-            outputs = [docker_build_output],
+            outputs = [docker_build_output]
         )
 
         pipeline.add_stage(
             stage_name = 'Docker-Push-ECR',
-            actions = [docker_build_action],
+            actions = [docker_build_action]
+        )
+
+        pipeline.add_stage(
+            stage_name = 'Deploy-Test',
+            actions = [
+                codepipeline_actions.EcsDeployAction(
+                    action_name = 'Deploy-Fargate-Test',
+                    service = test_app_fargate.service,
+                    input = docker_build_output
+                )
+            ]
         )
 
         CfnOutput(
